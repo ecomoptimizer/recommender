1ad2495d34dc708ff6a5e9a272b80de42ea3e871
ecomoptimizer <120761538+ecomoptimizer@users.noreply.github.com>
2024-02-29 07:11:27 -0700

    Update generate.py

--- src/generate.py
+++ src/generate.py
@@ -10,51 +10,54 @@ project_root = script_dir.parent
 # Add the project's root directory to sys.path
 sys.path.append(str(project_root))
 
-
 import streamlit as st
-from langchain.llms import OpenAI
-from langchain.chat_models import ChatOpenAI
-from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
-from langchain.output_parsers import PydanticOutputParser
-from langchain.schema import HumanMessage
+from langchain.llms import openai
+from langchain.chat_models import chatopenai
+from langchain.prompts import prompttemplate
+from langchain.chains import llmchain
+from langchain.output_parsers import pydanticoutputparser
+from langchain.schema import humanmessage
 
 from dotenv import dotenv_values
 
 from prompts.prompt import get_prompt
-from src.output import Campaign
+from src.output import campaign
 from src.utils import get_brand_tone_and_voice, format_json_to_multiline_string
 
 # config = dotenv_values(".env")
 
-# OPENAI_API_KEY = config["OPENAI_API_KEY"]
+# openai_api_key = config["openai_api_key"]
 
-def generate_emails(inputs, OPENAI_API_KEY):
+def generate_emails(inputs, api_key):
     try:
-        tone_and_voice = get_brand_tone_and_voice(inputs.tone_and_voice.tone_and_voice_from_website, OPENAI_API_KEY)
-        inputs.tone_and_voice.tone_and_voice_from_website = tone_and_voice 
+        tone_and_voice = get_brand_tone_and_voice(inputs.tone_and_voice.tone_and_voice_from_website, api_key)
+        inputs.tone_and_voice.tone_and_voice_from_website = tone_and_voice
     except AttributeError:
-        print("error")  
+        print("Error")
 
     string_inputs = format_json_to_multiline_string(inputs)
 
     with open("prompts/p2.prompt", "r") as f:
         template = f.read()
 
-    parser = PydanticOutputParser(pydantic_object=Campaign)
+    parser = pydanticoutputparser(pydantic_object=campaign)
 
-    prompt = PromptTemplate(
+    prompt = prompttemplate(
         template=template,
         input_variables=["inputs"],
         partial_variables={"output_format": parser.get_format_instructions()}
     )
 
-    llm = OpenAI(openai_api_key=OPENAI_API_KEY, max_tokens=3000, presence_penalty=1)
-    chat = ChatOpenAI(openai_api_key=OPENAI_API_KEY, max_tokens=3000)
+    if api_key == "openai":
+        llm = openai(openai_api_key=api_key, max_tokens=3000, presence_penalty=1)
+    elif api_key == "gemini-pro":
+        llm = openai(openai_api_key=api_key, max_tokens=3000, presence_penalty=1)
+
+    chat = chatopenai(openai_api_key=api_key, max_tokens=3000)
 
     formatted_input = prompt.format(inputs=string_inputs)
     # print("-"*30)
-    # print("Formatted Input:")
+    # print("formatted input:")
     # print(formatted_input)
     # print("-"*30)
 
