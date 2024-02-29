f6e836093223b1a7727cd9e516ce6c8c92bac4de
ecomoptimizer <120761538+ecomoptimizer@users.noreply.github.com>
2024-02-29 07:12:31 -0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -4,31 +4,31 @@ script_dir = Path(__file__).resolve().parent
 project_root = script_dir.parent
 sys.path.append(str(project_root))
 
-
 import uvicorn
-from fastapi import FastAPI, Header
+from fastapi import FastAPI
 import json
 
-from backend.inputs import Inputs
+from backend.inputs import inputs
 from src.generate import generate_emails
 
 app = FastAPI()
 
 @app.get("/")
 def index():
-    return {"message": "Hello, World"}
+    return {"message": "hello, world"}
 
 @app.post("/generate")
-def generate(inputs: Inputs):
+def generate(inputs: inputs):
     email_inputs = inputs.email_inputs
-    OPENAI_API_KEY = inputs.api_key
-    answer = generate_emails(email_inputs, OPENAI_API_KEY)
-    return answer
+    api_key = inputs.api_key
 
+    if api_key == "openai":
+        api_key = "google-generativeai" # Replace OpenAI key with Google GenerativeAI key
+    elif api_key == "gemini-pro":
+        api_key = "gemini-pro"  # Keep Gemini-Pro key as is
+
+    answer = generate_emails(email_inputs, api_key)
+    return answer
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
-
-
-
-    
